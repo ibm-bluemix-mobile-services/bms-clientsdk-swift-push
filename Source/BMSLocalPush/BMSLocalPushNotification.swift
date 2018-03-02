6f1ef43181ef18863db7c6dd75e44a1b40613563
Ananth <agirijak@in.ibm.com>
2018-03-02 11:49:17 +0530

    removed unwanted code

--- Source/BMSLocalPush/BMSLocalPushNotification.swift
+++ Source/BMSLocalPush/BMSLocalPushNotification.swift
@@ -102,25 +102,3 @@ class BMSLocalPushNotification: NSObject {
 
 }
 
-@available(iOS 10.0, *)
-extension UNNotificationAttachment {
-
-    /// Save the image to disk
-    static func create(imageFileIdentifier: String, data: NSData, options: [NSObject : AnyObject]?) -> UNNotificationAttachment? {
-        let fileManager = FileManager.default
-        let tmpSubFolderName = ProcessInfo.processInfo.globallyUniqueString
-        let tmpSubFolderURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(tmpSubFolderName, isDirectory: true)
-        
-        do {
-            try fileManager.createDirectory(at: tmpSubFolderURL!, withIntermediateDirectories: true, attributes: nil)
-            let fileURL = tmpSubFolderURL?.appendingPathComponent(imageFileIdentifier)
-            try data.write(to: fileURL!, options: [])
-            let imageAttachment = try UNNotificationAttachment(identifier: imageFileIdentifier, url: fileURL!, options: options)
-            return imageAttachment
-        } catch let error {
-            print("error \(error)")
-        }
-        return nil
-    }
-
-}
